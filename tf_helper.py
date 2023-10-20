6073ce21947eca5002bbfd95eb0f959d51a039ff
Til <t.widmann@qaamgo.com>
2023-10-20 09:48:24 +0200

    Add plot_fine_tuning

--- tf_helper.py
+++ tf_helper.py
@@ -152,3 +152,38 @@ def get_random_examples(train_dataset):
       plt.imshow(images[i].numpy().astype("uint8"))
       plt.title(class_names[labels[i]])
       plt.axis("off")
+
+# function to compare histories
+def plot_fine_tuning_epochs(original_history, new_history, initial_epochs):
+  """
+  Plots the Fine tuning epochs against the fine tuning epochs
+  """
+  acc = original_history.history["accuracy"]
+  loss = original_history.history["loss"]
+
+  val_acc = original_history.history["val_accuracy"]
+  val_loss = original_history.history["val_loss"]
+
+  total_acc = acc + new_history.history["accuracy"]
+  total_loss = loss + new_history.history["loss"]
+  
+  total_val_acc = val_acc + new_history.history["val_accuracy"]
+  total_val_loss = val_loss + new_history.history["val_loss"]
+
+  #Plot for accuracy
+  plt.figure(figsize=(8,8))
+  plt.subplot(2,1,1)
+  plt.plot(total_acc, label="Training Accuracy")
+  plt.plot(total_val_acc, label="Val Accuracy")
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label="Start fine tuning")
+  plt.legend(loc="lower right")
+  plt.title("Training and Validation Accuracy")
+  
+  # Plot for loss
+  plt.figure(figsize=(8,8))
+  plt.subplot(2,1,2)
+  plt.plot(total_loss, label="Training Loss")
+  plt.plot(total_val_loss, label="Val Loss")
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label="Start fine tuning")
+  plt.legend(loc="lower right")
+  plt.title("Training and Validation Loss")
